a77b628f9865cd4f01036ff809d99e608970ea85
Nehemiah51000 <nehemiahmwangi25@gmail.com>
2026-07-13 10:42:44 +0300

    FEAT: wire real dashboard layout with navigation links

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
-import DashboardPage from './pages/DashboardPage';
-import LoginPage from './pages/LoginPage';
+import { LoginPage } from './pages/LoginPage';
+import { DashboardPage } from './pages/DashboardPage';
+import { ProvidersPage } from './pages/ProvidersPage';
 import { ProtectedRoute } from './routes/ProtectedRoute';
+import { DashboardLayout } from './layout/DashboardLayout';
 
 function App() {
   return (
     <Routes>
       <Route path='/login' element={<LoginPage />} />
       <Route element={<ProtectedRoute />}>
-        <Route path='/' element={<DashboardPage />} />
-        {/* /clients, /providers, /transactions get added as we build each */}
+        <Route element={<DashboardLayout />}>
+          <Route path='/' element={<DashboardPage />} />
+          <Route path='/providers' element={<ProvidersPage />} />
+          {/* /clients, /transactions added as we build each */}
+        </Route>
       </Route>
     </Routes>
   );
---
+++ src/layout/DashboardLayout.tsx
@@ -0,0 +1,30 @@
+import { Outlet } from 'react-router-dom';
+import { LayoutDashboard, Users, Building2, Receipt } from 'lucide-react';
+import { AppShell } from './AppShell/AppShell';
+
+const navLinks = [
+  {
+    to: '/',
+    label: 'Dashboard',
+    icon: <LayoutDashboard className='h-4 w-4' />,
+  },
+  { to: '/clients', label: 'Clients', icon: <Users className='h-4 w-4' /> },
+  {
+    to: '/providers',
+    label: 'Providers',
+    icon: <Building2 className='h-4 w-4' />,
+  },
+  {
+    to: '/transactions',
+    label: 'Transactions',
+    icon: <Receipt className='h-4 w-4' />,
+  },
+];
+
+export function DashboardLayout() {
+  return (
+    <AppShell links={navLinks}>
+      <Outlet />
+    </AppShell>
+  );
+}
